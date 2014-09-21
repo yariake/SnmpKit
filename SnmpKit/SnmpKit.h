c34ac67ef365179bd92c2d2cc0aa530dc27dc8de
有明 義明 <yoshiaki.ariake@gmail.com>
2014-09-21 13:03:11 +0900

    Initial Commit

---
+++ SnmpKit/SnmpKit.h
@@ -0,0 +1,19 @@
+//
+//  SnmpKit.h
+//  SnmpKit
+//
+//  Created by 有明 義明 on 2014/09/21.
+//  Copyright (c) 2014年 Tetoatom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SnmpKit.
+FOUNDATION_EXPORT double SnmpKitVersionNumber;
+
+//! Project version string for SnmpKit.
+FOUNDATION_EXPORT const unsigned char SnmpKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnmpKit/PublicHeader.h>
+
+
---
+++ SnmpKitTests/SnmpKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  SnmpKitTests.swift
+//  SnmpKitTests
+//
+//  Created by 有明 義明 on 2014/09/21.
+//  Copyright (c) 2014年 Tetoatom. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SnmpKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
